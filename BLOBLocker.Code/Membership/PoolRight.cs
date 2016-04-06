96e018fb4b658c3141e5b80dc13f057536409246
Gurkenschreck <mariogiugno@gmx.de>
2016-04-07 00:33:09 +0200

    Added PoolRight enum

---
+++ BLOBLocker.Code/Membership/PoolRight.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLOBLocker.Code.Membership
+{
+    public enum PoolRight : int
+    {
+        InviteUser = 1 << 0,
+        Removeuser = 1 << 1,
+        ManageModules = 1 << 2,
+        AssignRights = 1 << 3,
+        ReadFileStorage = 1 << 4,
+        UploadFileStorage = 1 << 5,
+        ReadChat = 1 << 6,
+        WriteChat = 1 << 7,
+        ReadLinkRepository = 1 << 8,
+        WriteLinkRepository = 1 << 9,
+        RemoveAssignedMemory = 1 << 10,
+        ChangeTitle = 1 << 11,
+        ChangeDescription = 1 << 12,
+        ChangeBanner = 1 << 13,
+        ManageFileStorage = 1 << 14,
+        ManageChat = 1 << 15,
+        ViewPoolLog = 1 << 16
+    }
+}
