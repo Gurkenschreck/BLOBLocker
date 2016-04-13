d85d5913cfaa23f3c086a2a19aa5ee76c840b71d
Gurkenschreck <mariogiugno@gmx.de>
2016-04-13 18:37:02 +0200

    Added Scripts Folder. Inserted latest backup scripts.

---
+++ BLOBLocker.Entities/Scripts/AdminTool/160413_dbo.Roles.data.sql
@@ -0,0 +1,5 @@
+SET IDENTITY_INSERT [dbo].[Roles] ON
+INSERT INTO [dbo].[Roles] ([ID], [Definition]) VALUES (1, N'Translator')
+INSERT INTO [dbo].[Roles] ([ID], [Definition]) VALUES (2, N'Administrator')
+INSERT INTO [dbo].[Roles] ([ID], [Definition]) VALUES (3, N'Moderator')
+SET IDENTITY_INSERT [dbo].[Roles] OFF
---
+++ BLOBLocker.Entities/Scripts/WebApp/160413_dbo.AccountRoles.data.sql
@@ -0,0 +1,4 @@
+SET IDENTITY_INSERT [dbo].[AccountRoles] ON
+INSERT INTO [dbo].[AccountRoles] ([ID], [Definition]) VALUES (1, N'Basic')
+INSERT INTO [dbo].[AccountRoles] ([ID], [Definition]) VALUES (2, N'Administrator')
+SET IDENTITY_INSERT [dbo].[AccountRoles] OFF
