1bc48b4e74a3b9f0dfbf46afbf6fe5e7e34d7a4e
Gurkenschreck <mariogiugno@gmx.de>
2016-04-21 00:02:28 +0200

    Added SecurityController, Info and Peek ActionMethods. Added MinimalLayout

---
+++ BLOBLocker.WebApp/Controllers/SecurityController.cs
@@ -0,0 +1,43 @@
+﻿using BLOBLocker.Code.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BLOBLocker.WebApp.Controllers
+{
+    [AllowAnonymous]
+    public class SecurityController : BaseController
+    {
+        //
+        // GET: /Security/Info
+        [HttpGet]
+        public ActionResult Info(bool? noLayout)
+        {
+            if (noLayout == null)
+                noLayout = false;
+
+            ViewBag.NoLayout = noLayout;
+
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult Peek(string url)
+        {
+            if (string.IsNullOrWhiteSpace(url))
+            {
+                url = "/Security/Info?noLayout=true";
+            }
+            else
+            {
+                if (!url.StartsWith("https"))
+                    url = url.Replace("http", "https");
+            }
+            ViewBag.URL = url;
+            return View();
+        }
+    }
+}
