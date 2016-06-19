9c1cc4a00a12187e28853f4e1b7b05352aa1d70d
Gurkenschreck <mariogiugno@gmx.de>
2016-06-19 21:21:15 +0200

    task #47 fixed deformed foundation 6 layout

--- BLOBLocker.Code/Web/Helper/Localized.cs
+++ BLOBLocker.Code/Web/Helper/Localized.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Mvc.Html;
+using BLOBLocker.Code.Extention;
 
 namespace BLOBLocker.Code.Web.Helper
 {
@@ -13,40 +14,40 @@ namespace BLOBLocker.Code.Web.Helper
     {
         public static MvcHtmlString LocalizedActionLink(this HtmlHelper htmlHelper, string resourceKey, string action)
         {
-            return htmlHelper.ActionLink(HttpContext.GetGlobalResourceObject(null, resourceKey).ToString(), action);
+            return htmlHelper.ActionLink(HttpContext.GetGlobalResourceObject(null, resourceKey).As<string>(), action);
         }
 
         public static MvcHtmlString LocalizedActionLink(this HtmlHelper htmlHelper, string resourceKey, string action, string controller)
         {
-            return htmlHelper.ActionLink(HttpContext.GetGlobalResourceObject(null, resourceKey).ToString(),
+            return htmlHelper.ActionLink(HttpContext.GetGlobalResourceObject(null, resourceKey).As<string>(),
                 action, controller);
         }
 
         public static MvcHtmlString LocalizedActionLink(this HtmlHelper htmlHelper, string resourceKey, string action, object routeValues)
         {
-            return htmlHelper.ActionLink(HttpContext.GetGlobalResourceObject(null, resourceKey).ToString(),
+            return htmlHelper.ActionLink(HttpContext.GetGlobalResourceObject(null, resourceKey).As<string>(),
                 action, routeValues);
         }
         public static MvcHtmlString LocalizedActionLink(this HtmlHelper htmlHelper, string resourceKey, string action, object routeValues, object htmlAttributes)
         {
-            return htmlHelper.ActionLink(HttpContext.GetGlobalResourceObject(null, resourceKey).ToString(),
+            return htmlHelper.ActionLink(HttpContext.GetGlobalResourceObject(null, resourceKey).As<string>(),
                 action, routeValues, htmlAttributes);
         }
 
         public static MvcHtmlString LocalizedActionLink(this HtmlHelper htmlHelper, string resourceKey, string action, string controller, object routeValues, object htmlAttributes)
         {
-            return htmlHelper.ActionLink(HttpContext.GetGlobalResourceObject(null, resourceKey).ToString(),
+            return htmlHelper.ActionLink(HttpContext.GetGlobalResourceObject(null, resourceKey).As<string>(),
                 action, controller, routeValues, htmlAttributes);
         }
 
         public static MvcHtmlString LocalizedLabel(this HtmlHelper htmlHelper, string key)
         {
-            return htmlHelper.Label(HttpContext.GetGlobalResourceObject(null, key).ToString());
+            return htmlHelper.Label(HttpContext.GetGlobalResourceObject(null, key).As<string>());
         }
 
         public static MvcHtmlString LocalizedLabel(this HtmlHelper htmlHelper, string key, object htmlAttributes)
         {
-            return htmlHelper.Label(HttpContext.GetGlobalResourceObject(null, key).ToString(),
+            return htmlHelper.Label(HttpContext.GetGlobalResourceObject(null, key).As<string>(),
                 htmlAttributes);
         }
     }
