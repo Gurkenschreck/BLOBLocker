1a30b7cae008cc2e070a9b0dd8b1ee84682aa082
Gurkenschreck <mariogiugno@gmx.de>
2016-06-30 21:30:33 +0200

    Added first ASP.NET Web Api Controller with test functionality

---
+++ BLOBLocker.WebApp/API/Controllers/SecurityController.cs
@@ -0,0 +1,24 @@
+﻿using BLOBLocker.Entities.Models.WebApp;
+using BLOBLocker.WebApp.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace BLOBLocker.WebApp.API.Controllers
+{
+    public class SecurityController : ApiController
+    {
+        public SecurityInformation GetInformations()
+        {
+            return new SecurityInformation
+            {
+                IPV4Address = Request.Properties["MS_HttpContext"] != null ? ((HttpContextBase)Request.Properties["MS_HttpContext"]).Request.UserHostAddress : "" 
+            };
+        }
+    }
+}
---
+++ BLOBLocker.WebApp/API/Models/SecurityInformation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BLOBLocker.WebApp.API.Models
+{
+    public class SecurityInformation
+    {
+        public string IPV4Address { get; set; }
+    }
+}
