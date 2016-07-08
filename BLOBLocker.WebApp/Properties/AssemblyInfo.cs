2f13b87fd615302c1d175fe6423b75181b9bba6d
Gurkenschreck <mariogiugno@gmx.de>
2016-07-08 23:37:11 +0200

    Set proper rev numbers

--- BLOBLocker.WebApp/Properties/AssemblyInfo.cs
+++ BLOBLocker.WebApp/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.1.0.150")]
-[assembly: AssemblyFileVersion("0.1.0.150")]
+[assembly: AssemblyVersion("0.1.0.154")]
+[assembly: AssemblyFileVersion("0.1.0.154")]
